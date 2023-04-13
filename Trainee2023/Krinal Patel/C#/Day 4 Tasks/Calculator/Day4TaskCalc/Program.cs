416e66b6aecaf82448e35ee5179aa2bd52eccc12
Krinal Patel <krinalp1403@gmail.com>
2023-04-13 09:17:34 +0530

    C# Exam Folder Added

---
+++ Trainee2023/Krinal Patel/C#/Day 4 Tasks/Calculator/Day4TaskCalc/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Day4TaskCalc
+{
+
+    class Program
+    {
+
+        //Calculator
+        static double addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static double substract(double no1, double no2)
+        {
+            return no1 - no2;
+        }
+        static double multiply(double no1, double no2)
+        {
+            return no1 * no2;
+        }
+        static double division(double no1, double no2)
+        {
+            if (no2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return no1 / no2;
+        }
+        static double modulus(double no1, double no2)
+        {
+            return no1 % no2;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            do
+            {
+                Console.WriteLine("\n------------------------- MENU ---------------------------\n");
+
+                Console.WriteLine("1. Calculator");
+                Console.WriteLine("2. Student Portal");
+
+                Console.WriteLine("0. Exit");
+
+
+
+
+                Console.WriteLine("\n--------------------------------------------------------\n");
+                Console.Write("Enter Your Choice : ");
+
+                var task = Convert.ToInt32(Console.ReadLine());
+
+                switch (task)
+                {
+                    case 1:
+
+                        try
+                        {
+                            Console.Write("Enter Number 1 : ");
+                            var no1 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter Number 2 : ");
+                            var no2 = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("The Addition of {0} and {1} is: {2} ", no1, no2, addition(no1, no2));
+                            Console.WriteLine("The Substraction of {0} and {1} is: {2} ", no1, no2, substract(no1, no2));
+                            Console.WriteLine("The Multiplication of {0} and {1} is: {2} ", no1, no2, multiply(no1, no2));
+                            Console.WriteLine("The Division of {0} and {1} is: {2} ", no1, no2, division(no1, no2));
+                            Console.WriteLine("The Modulus of {0} and {1} is: {2} ", no1, no2, modulus(no1, no2));
+
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Done in another file named Student");
+
+
+                        break;
+
+                    case 0:
+                        Environment.Exit(1);// exit
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Enter 1 to 7");
+                        Console.ReadLine(); //waits for enter button to be pressed to exit console
+                        break;
+
+                }
+
+            }
+            while (true);
+
+        }
+    }
+
+
+}
+
---
+++ Trainee2023/Krinal Patel/C#/OOP Day 1 Practice/OOP Practice/OOP Practice/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using static OOP_Practice.ROI_Calc;
+using static OOP_Practice.Uber;
+
+namespace OOP_Practice
+{
+    internal class Program
+    {
+        public class Player
+        {
+     
+         //Encapsulation : Encapsulation is the process of hiding internal details of an object from the outside world and providing a public interface for accessing it.
+         //Here data members of Player class is kept private but getter and setter methods of it is kept public for open access..  
+         
+            private string name = "KP";
+            private int runs;
+
+            public string Name                                  //Name getter setter is Public
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public int Runs                                    //Run getter setter is Public
+            {
+                get { return runs; }
+                set { runs = value; }
+            }
+
+      
+
+        }
+        class Team : Player                     //Player is base class and it is inherited by Team class that makes use of Name property of Player class 
+        {
+            private new Player Name
+           
+            { get
+                {
+                        return Name;
+                } }
+
+            //Overloading   : Method with same name but Different Parameters
+            public void Play(string team1, string team2)                                            
+            {
+                Console.WriteLine("{0} vs {1} - Let the game begin!", team1, team2);
+            }
+
+            public void Play(string team1, string team2, int overs)
+            {
+                Console.WriteLine("{0} vs {1} - {2} overs game starts now!", team1, team2, overs);
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            //CRICKET
+            Team team = new Team();
+
+            //Inheritance
+            Console.WriteLine("Player Name (Inherited) : "+team.Name);
+
+            //Overloading : It takes the method as per the parameters passed
+
+            team.Play("India","Pakistan");
+            team.Play("Australia", "India", 50);
+            Console.ReadLine();
+
+
+
+            //UBER
+            Driver driver = new Driver();
+
+            //Inheritance
+            Console.WriteLine("Uber Customer Name (Inherited) : " + driver.CName);
+            Console.WriteLine("Uber Customer Region (Inherited) : " + driver.Region);
+            Console.ReadLine();
+
+            //ROI CALCULATOR
+            InputValues calc = new InputValues();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
