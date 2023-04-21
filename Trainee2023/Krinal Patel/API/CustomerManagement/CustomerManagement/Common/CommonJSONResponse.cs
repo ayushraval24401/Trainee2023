98190d4a43699597c1da2d0a74d7e44217fef043
Krinal Patel <krinalp1403@gmail.com>
2023-04-21 11:43:26 +0530

    API Crud with File System and JWT Authorization Completed

---
+++ Trainee2023/Krinal Patel/API/CustomerManagement/CustomerManagement/Common/CommonJSONResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerManagement.Common
+{
+    public class CommonJSONResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public dynamic data { get; set; }
+    }
+}
---
+++ Trainee2023/Krinal Patel/API/CustomerManagementAuthuntication/CustomerManagement/Common/CommonMethods.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata;
+using System.Security.Claims;
+using System.Text;
+using System;
+using DataAccess;
+using Customers;
+
+namespace CustomerManagement.Common
+{
+
+    public static class CommonMethods
+    {
+        public static string GenerateJSONWebToken(User user,string secret)
+        {
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, $"{user.Email}"),
+                new Claim(ClaimTypes.NameIdentifier, $"{user.UserId}"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            var tokeOptions = new JwtSecurityToken(
+                issuer: "https://localhost:44348",
+                audience: "https://localhost:44348",
+                claims: authClaims,
+                expires: DateTime.UtcNow.AddMinutes(59),
+                signingCredentials: signinCredentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+        }
+
+     
+    }
+}
